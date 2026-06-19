1b195f7f6d5ddda965edb3c11563b768e3ad88de
한승환 <coderrornotfound@gmail.com>
2026-06-19 12:26:25 +0900

    fix: product 데이터 price 문자열 -> 숫자로 변경

--- src/data/products.js
+++ src/data/products.js
@@ -2,7 +2,7 @@ export const products = [
   {
     id: 1,
     name: "용인셔츠",
-    price: "10000",
+    price: 10000,
     category: "옷",
     image: "images/yongin-shirts.svg",
     description: "용인이 사진이 들어간 이쁜 셔츠",
@@ -12,7 +12,7 @@ export const products = [
   {
     id: 2,
     name: "용인모자",
-    price: "19800",
+    price: 19800,
     category: "모자",
     image: "images/yongin-cap.svg",
     description: "용인이 사진이 들어간 귀여운 모자",
@@ -22,7 +22,7 @@ export const products = [
   {
     id: 3,
     name: "용인후드티",
-    price: "38900",
+    price: 38900,
     category: "옷",
     image: "images/yongin-hood.svg",
     description: "용인이 사진이 들어간 이쁜 후드티",
@@ -32,7 +32,7 @@ export const products = [
   {
     id: 4,
     name: "용인포카",
-    price: "89900",
+    price: 89900,
     category: "포토카드",
     image: "images/yongin-photocard.svg",
     description: "용인이 사진이 들어간 멋진 포토카드",
